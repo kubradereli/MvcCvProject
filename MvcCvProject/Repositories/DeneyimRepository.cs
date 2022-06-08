c4142b7e55b044177bf3947176a79d36af5fec6b
kubradereli <0kubradereli@gmail.com>
2022-06-08 12:28:11 +0300

    Admin paneli eklendi. Deneyimler kısmının listeleme, ekleme, silme ve güncelleme işlemleri tamamlandı.

---
+++ MvcCvProject/Repositories/DeneyimRepository.cs
@@ -0,0 +1,13 @@
+﻿using MvcCvProject.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcCvProject.Repositories
+{
+    public class DeneyimRepository : GenericRepository<TblDeneyimlerim>
+    {
+
+    }
+}
