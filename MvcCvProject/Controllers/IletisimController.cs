d9f826bb4840389848989c03067aade2f5451a70
kubradereli <0kubradereli@gmail.com>
2022-06-10 10:54:08 +0300

    İletişim sayfasında mesaj listeleme işlemi tamamlandı.

---
+++ MvcCvProject/Controllers/IletisimController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcCvProject.Models.Entity;
+using MvcCvProject.Repositories;
+
+namespace MvcCvProject.Controllers
+{
+    public class IletisimController : Controller
+    {
+        // GET: Iletisim
+
+        GenericRepository<TblIletisim> repo = new GenericRepository<TblIletisim>();
+
+        public ActionResult Index()
+        {
+            var mesajlar = repo.List();
+            return View(mesajlar);
+        }
+    }
+}
